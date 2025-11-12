a721041ffe506f9ebc6236abd1e38e352aecede2
Sammy <96604660+itsmasso@users.noreply.github.com>
2025-11-11 22:39:56 -0800

    Update Program.cs

--- Recipedia/Recipedia/Program.cs
+++ Recipedia/Recipedia/Program.cs
@@ -9,7 +9,6 @@ using Recipedia.Data.Services;
 using Recipedia.Models;
 using Rotativa.AspNetCore;
 
-
 var builder = WebApplication.CreateBuilder(args);
 
 var databaseUrl = Environment.GetEnvironmentVariable("DATABASE_URL");
@@ -18,14 +17,16 @@ if (!string.IsNullOrEmpty(databaseUrl))
     var uri = new Uri(databaseUrl);
     var username = uri.UserInfo.Split(':')[0];
     var password = uri.UserInfo.Split(':')[1];
-
     var connectionString = $"Host={uri.Host};Port={uri.Port};Database={uri.AbsolutePath.TrimStart('/')};Username={username};Password={password};SSL Mode=Require;Trust Server Certificate=true";
-
     builder.Configuration["ConnectionStrings:DefaultConnection"] = connectionString;
 }
+
 // Add services to the container.
 builder.Services.AddControllersWithViews();
-builder.Services.AddDbContext<RecipediaAppContext>(options => options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+builder.Services.AddDbContext<RecipediaAppContext>(options =>
+    options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
+
 builder.Services.AddHttpClient<IGeminiService, GeminiService>();
 builder.Services.AddHttpClient<GoogleSearchEngineService>();
 builder.Services.AddHttpClient<SpoonacularService>();
@@ -39,48 +40,61 @@ builder.Services.AddIdentity<User, IdentityRole>(options =>
     options.Password.RequireUppercase = false;
     options.Password.RequireNonAlphanumeric = false;
     options.Password.RequiredLength = 6;
-
     // User settings
     options.User.RequireUniqueEmail = true;
 })
 .AddEntityFrameworkStores<RecipediaAppContext>()
 .AddDefaultTokenProviders();
 
+// Data Protection - store keys in database instead of file system
 builder.Services.AddDataProtection()
-    .PersistKeysToFileSystem(new DirectoryInfo("/keys")) 
+    .PersistKeysToFileSystem(new DirectoryInfo("/keys"))
     .SetApplicationName("Recipedia");
-
-builder.Services.AddControllersWithViews();
 
 // Configure Authentication
 builder.Services.AddAuthentication(options =>
 {
     options.DefaultScheme = CookieAuthenticationDefaults.AuthenticationScheme;
 })
-.AddCookie()
+.AddCookie(options =>
+{
+    options.Cookie.SameSite = SameSiteMode.Lax;
+    options.Cookie.SecurePolicy = CookieSecurePolicy.Always;
+    options.Cookie.IsEssential = true;
+})
 .AddGoogle(googleOptions =>
 {
     googleOptions.ClientId = builder.Configuration["Authentication:Google:ClientId"];
     googleOptions.ClientSecret = builder.Configuration["Authentication:Google:ClientSecret"];
     googleOptions.SaveTokens = true;
+    googleOptions.CallbackPath = "/signin-google";
+
+    // Fix correlation cookie issue
+    googleOptions.CorrelationCookie.SameSite = SameSiteMode.Lax;
+    googleOptions.CorrelationCookie.SecurePolicy = CookieSecurePolicy.Always;
+    googleOptions.CorrelationCookie.IsEssential = true;
 });
+
 // Configure application cookie
 builder.Services.ConfigureApplicationCookie(options =>
 {
     options.LoginPath = "/Account/Login";
     options.LogoutPath = "/Account/Logout";
     options.AccessDeniedPath = "/Account/AccessDenied";
+    options.Cookie.SameSite = SameSiteMode.Lax;
+    options.Cookie.SecurePolicy = CookieSecurePolicy.Always;
+    options.Cookie.IsEssential = true;
 });
-var isProd = builder.Environment.IsProduction();
 
+var isProd = builder.Environment.IsProduction();
 if (isProd)
-{   
+{
     // Use port provided in env or 8080
     builder.WebHost.UseUrls("http://0.0.0.0:" + (Environment.GetEnvironmentVariable("PORT") ?? "8080"));
 }
 
-
 var app = builder.Build();
+
 // Auto-run migrations on startup
 using (var scope = app.Services.CreateScope())
 {
@@ -96,25 +110,24 @@ using (var scope = app.Services.CreateScope())
         logger.LogError(ex, "An error occurred while migrating the database.");
     }
 }
+
 RotativaConfiguration.Setup(app.Environment.WebRootPath, "Rotativa/Windows");
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
-	app.UseExceptionHandler("/Home/Error");
-	
-	app.UseHsts();
+    app.UseExceptionHandler("/Home/Error");
+    app.UseHsts();
 }
 
 app.UseHttpsRedirection();
 app.UseStaticFiles();
-
 app.UseRouting();
 app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllerRoute(
-	name: "default",
-	pattern: "{controller=FindRecipe}/{action=Index}/{id?}");
+    name: "default",
+    pattern: "{controller=FindRecipe}/{action=Index}/{id?}");
 
 app.Run();
